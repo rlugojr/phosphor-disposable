8ab5cbba7dfcb7e9b75fec5ca48fd87e3972bce4
S. Chris Colbert <sccolbert@gmail.com>
2015-08-23 19:28:05 -0400

    checkin source and tests

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,123 @@
 | The full license is in the file LICENSE, distributed with this software.
 |----------------------------------------------------------------------------*/
 'use strict';
+
+/**
+ * An object which implements the disposable pattern.
+ */
+export
+interface IDisposable {
+  /**
+   * Dispose of the resources held by the object.
+   *
+   * #### Notes
+   * It is generally unsafe to use the object after it is disposed.
+   *
+   * If an object's `dispose` method is called more than once, all
+   * calls made after the first will be a no-op.
+   */
+  dispose(): void;
+}
+
+
+/**
+ * A disposable object which delegates to a callback.
+ */
+export
+class DisposableDelegate implements IDisposable {
+  /**
+   * Construct a new disposable delegate.
+   *
+   * @param callback - The callback to invoke when the object is
+   *   disposed.
+   */
+  constructor(callback: () => void) {
+    this._callback = callback;
+  }
+
+  /**
+   * Dispose of the resources held by the object.
+   */
+  dispose(): void {
+    var callback = this._callback;
+    this._callback = null;
+    if (callback) callback();
+  }
+
+  private _callback: () => void;
+}
+
+
+/**
+ * An object which manages a collection of disposables.
+ *
+ * #### Notes
+ * Items will be disposed in the order they are added to the set.
+ */
+export
+class DisposableSet implements IDisposable {
+  /**
+   * Construct a new disposable set.
+   *
+   * @param items - The initial disposable items for the set.
+   */
+  constructor(items?: IDisposable[]) {
+    if (items) items.forEach(item => this._set.add(item));
+  }
+
+  /**
+   * Dispose of the resources held by the object.
+   */
+  dispose(): void {
+    var set = this._set;
+    this._set = null;
+    if (set) set.forEach(item => item.dispose());
+  }
+
+  /**
+   * Add a disposable item to the set.
+   *
+   * @param item - The disposable item to add to the set. If the item
+   *   is already contained in the set, this is a no-op.
+   *
+   * #### Notes
+   * If the set is already disposed, this will throw an error.
+   */
+  add(item: IDisposable): void {
+    if (!this._set) {
+      throw new Error('object is disposed');
+    }
+    this._set.add(item);
+  }
+
+  /**
+   * Remove a disposable item from the set.
+   *
+   * @param item - The disposable item to remove from the set. If the
+   *   item does not exist in the set, this is a no-op.
+   *
+   * #### Notes
+   * If the set is already disposed, this will throw an error.
+   */
+  remove(item: IDisposable): void {
+    if (!this._set) {
+      throw new Error('object is disposed');
+    }
+    this._set.delete(item);
+  }
+
+  /**
+   * Clear all disposable items from the set.
+   *
+   * #### Notes
+   * If the set is already disposed, this will throw an error.
+   */
+  clear(): void {
+    if (!this._set) {
+      throw new Error('object is disposed');
+    }
+    this._set.clear();
+  }
+
+  private _set = new Set<IDisposable>();
+}
--- test/testdisposable.ts
+++ test/testdisposable.ts
@@ -6,3 +6,257 @@
 | The full license is in the file LICENSE, distributed with this software.
 |----------------------------------------------------------------------------*/
 'use strict';
+
+import expect = require('expect.js');
+
+import {
+  DisposableDelegate, DisposableSet, IDisposable
+} from '../lib/index';
+
+
+class TestDisposable implements IDisposable {
+
+  count = 0;
+
+  dispose(): void {
+    this.count++;
+  }
+}
+
+
+describe('phosphor-disposable', () => {
+
+  describe('DisposableDelegate', () => {
+
+    describe('constructor', () => {
+
+      it('should accept a callback', () => {
+        var delegate = new DisposableDelegate(() => { });
+        expect(delegate instanceof DisposableDelegate).to.be(true);
+      });
+
+    });
+
+    describe('#dispose()', () => {
+
+      it('should invoke a callback when disposed', () => {
+        var called = false;
+        var delegate = new DisposableDelegate(() => called = true);
+        expect(called).to.be(false);
+        delegate.dispose();
+        expect(called).to.be(true);
+      });
+
+      it('should ignore multiple calls to `dispose`', () => {
+        var count = 0;
+        var delegate = new DisposableDelegate(() => count++);
+        expect(count).to.be(0);
+        delegate.dispose();
+        delegate.dispose();
+        delegate.dispose();
+        expect(count).to.be(1);
+      });
+
+    });
+
+  });
+
+  describe('DisposableSet', () => {
+
+    describe('constructor', () => {
+
+      it('should accept no arguments', () => {
+        var set = new DisposableSet();
+        expect(set instanceof DisposableSet).to.be(true);
+      });
+
+      it('should accept an array of disposable items', () => {
+        var item1 = new TestDisposable();
+        var item2 = new TestDisposable();
+        var item3 = new TestDisposable();
+        var set = new DisposableSet([item1, item2, item3]);
+        expect(set instanceof DisposableSet).to.be(true);
+      });
+
+    });
+
+    describe('#dispose()', () => {
+
+      it('should dispose all items in the set', () => {
+        var item1 = new TestDisposable();
+        var item2 = new TestDisposable();
+        var item3 = new TestDisposable();
+        var set = new DisposableSet([item1, item2, item3]);
+        expect(item1.count).to.be(0);
+        expect(item2.count).to.be(0);
+        expect(item3.count).to.be(0);
+        set.dispose();
+        expect(item1.count).to.be(1);
+        expect(item2.count).to.be(1);
+        expect(item3.count).to.be(1);
+      });
+
+      it('should dipose items in the order they were added', () => {
+        var values: number[] = [];
+        var item1 = new DisposableDelegate(() => values.push(0));
+        var item2 = new DisposableDelegate(() => values.push(1));
+        var item3 = new DisposableDelegate(() => values.push(2));
+        var set = new DisposableSet([item1, item2, item3]);
+        expect(values).to.eql([]);
+        set.dispose();
+        expect(values).to.eql([0, 1, 2]);
+      });
+
+      it('should ignore multiple calls to `dispose`', () => {
+        var item1 = new TestDisposable();
+        var item2 = new TestDisposable();
+        var item3 = new TestDisposable();
+        var set = new DisposableSet([item1, item2, item3]);
+        expect(item1.count).to.be(0);
+        expect(item2.count).to.be(0);
+        expect(item3.count).to.be(0);
+        set.dispose();
+        set.dispose();
+        set.dispose();
+        expect(item1.count).to.be(1);
+        expect(item2.count).to.be(1);
+        expect(item3.count).to.be(1);
+      });
+
+    });
+
+    describe('#add()', () => {
+
+      it('should add items to the set', () => {
+        var item1 = new TestDisposable();
+        var item2 = new TestDisposable();
+        var item3 = new TestDisposable();
+        var set = new DisposableSet();
+        set.add(item1);
+        set.add(item2);
+        set.add(item3);
+        expect(item1.count).to.be(0);
+        expect(item2.count).to.be(0);
+        expect(item3.count).to.be(0);
+        set.dispose();
+        expect(item1.count).to.be(1);
+        expect(item2.count).to.be(1);
+        expect(item3.count).to.be(1);
+      });
+
+      it('should maintain insertion order', () => {
+        var values: number[] = [];
+        var item1 = new DisposableDelegate(() => values.push(0));
+        var item2 = new DisposableDelegate(() => values.push(1));
+        var item3 = new DisposableDelegate(() => values.push(2));
+        var set = new DisposableSet([item1]);
+        set.add(item2);
+        set.add(item3);
+        expect(values).to.eql([]);
+        set.dispose();
+        expect(values).to.eql([0, 1, 2]);
+      });
+
+      it('should ignore duplicate items', () => {
+        var values: number[] = [];
+        var item1 = new DisposableDelegate(() => values.push(0));
+        var item2 = new DisposableDelegate(() => values.push(1));
+        var item3 = new DisposableDelegate(() => values.push(2));
+        var set = new DisposableSet([item1]);
+        set.add(item2);
+        set.add(item3);
+        set.add(item3);
+        set.add(item2);
+        set.add(item1);
+        expect(values).to.eql([]);
+        set.dispose();
+        expect(values).to.eql([0, 1, 2]);
+      });
+
+      it('should throw if the set is disposed', () => {
+        var item = new TestDisposable();
+        var set = new DisposableSet();
+        set.dispose();
+        expect(() => set.add(item)).to.throwException();
+      });
+
+    });
+
+    describe('#remove()', () => {
+
+      it('should remove items from the set', () => {
+        var item1 = new TestDisposable();
+        var item2 = new TestDisposable();
+        var item3 = new TestDisposable();
+        var set = new DisposableSet([item1, item2, item3]);
+        expect(item1.count).to.be(0);
+        expect(item2.count).to.be(0);
+        expect(item3.count).to.be(0);
+        set.remove(item2);
+        set.dispose();
+        expect(item1.count).to.be(1);
+        expect(item2.count).to.be(0);
+        expect(item3.count).to.be(1);
+      });
+
+      it('should maintain insertion order', () => {
+        var values: number[] = [];
+        var item1 = new DisposableDelegate(() => values.push(0));
+        var item2 = new DisposableDelegate(() => values.push(1));
+        var item3 = new DisposableDelegate(() => values.push(2));
+        var set = new DisposableSet([item1, item2, item3]);
+        expect(values).to.eql([]);
+        set.remove(item1);
+        set.dispose();
+        expect(values).to.eql([1, 2]);
+      });
+
+      it('should ignore missing items', () => {
+        var values: number[] = [];
+        var item1 = new DisposableDelegate(() => values.push(0));
+        var item2 = new DisposableDelegate(() => values.push(1));
+        var item3 = new DisposableDelegate(() => values.push(2));
+        var set = new DisposableSet([item1, item2]);
+        expect(values).to.eql([]);
+        set.remove(item3);
+        set.dispose();
+        expect(values).to.eql([0, 1]);
+      });
+
+      it('should throw if the set is disposed', () => {
+        var item = new TestDisposable();
+        var set = new DisposableSet();
+        set.dispose();
+        expect(() => set.remove(item)).to.throwException();
+      });
+
+    });
+
+    describe('#clear()', () => {
+
+      it('remove all items from the set', () => {
+        var item1 = new TestDisposable();
+        var item2 = new TestDisposable();
+        var item3 = new TestDisposable();
+        var set = new DisposableSet([item1, item2, item3]);
+        expect(item1.count).to.be(0);
+        expect(item2.count).to.be(0);
+        expect(item3.count).to.be(0);
+        set.clear();
+        set.dispose();
+        expect(item1.count).to.be(0);
+        expect(item2.count).to.be(0);
+        expect(item3.count).to.be(0);
+      });
+
+      it('should throw if the set is disposed', () => {
+        var set = new DisposableSet();
+        set.dispose();
+        expect(() => set.clear()).to.throwException();
+      });
+
+    });
+
+  });
+
+});
