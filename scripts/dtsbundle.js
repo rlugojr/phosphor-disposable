ccd33b5ddf6187ea181cd20ccacb7be6e4da1d80
S. Chris Colbert <sccolbert@gmail.com>
2015-09-03 17:03:35 -0400

    update to TS 1.6

--- scripts/dtsbundle.js
+++
@@ -1,7 +0,0 @@
-require('dts-generator').generate({
-  name: 'phosphor-disposable',
-  main: 'phosphor-disposable/index',
-  baseDir: 'lib',
-  files: ['index.d.ts'],
-  out: 'lib/phosphor-disposable.d.ts',
-});
